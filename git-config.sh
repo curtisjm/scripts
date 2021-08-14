78953ad70860f852a1588d430ce4ecde32e21adc
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-08-13 21:35:37 -0700

    fix gcm install & add setup

--- git-config.sh
+++ git-config.sh
@@ -1,6 +1,11 @@
+git config --global user.name "Curtis Mitchell"
+git config --global user.email curtisjmitchell1@gmail.com
+
 curl -sSL https://packages.microsoft.com/config/ubuntu/21.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
 sudo apt update
 sudo apt install gcmcore
-git-credential-manager-core configure
+
+git config --global credential.helper manager-core
+git config --global credential.credentialStore cache
 
