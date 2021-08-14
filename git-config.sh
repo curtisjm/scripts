71c4fc1691c096c9220836ed2b1fea85dbcdbc77
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-08-14 15:12:30 -0700

    add pull merge

--- git-config.sh
+++ git-config.sh
@@ -1,5 +1,6 @@
 git config --global user.name "Curtis Mitchell"
 git config --global user.email curtisjmitchell1@gmail.com
+git config --global pull.rebase false
 
 curl -sSL https://packages.microsoft.com/config/ubuntu/21.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
